470e02433e3ffea01654ce4e4d0aa11e4735e4af
Mendel Gelfand <mendelgel@gmail.com>
2019-05-29 22:03:38 +0300

    в

--- main.ino
+++ main.ino
@@ -9,27 +9,44 @@
 #define PIN_SRV 13		// пин вывода: на сервопривод
 #define SENSITIVITY 180 // чувствительность
 
-Servo _srv;			  // сервопривод
-int l_phtrsr;		  // значения принимамые левым фоторезистором
-int r_phtrsr;		  // значения принимамые правым фоторезистором
-int srv_position = 0; // текущая позиция сервопривода
+Servo _srv;   // сервопривод
+int l_phtrsr; // значения принимамые левым фоторезистором
+int r_phtrsr; // значения принимамые правым фоторезистором
 
 void setup()
 {
-	_srv.attach(PIN_SRV);
+	_srv.attach(PIN_SRV); // определение переменной _srv на получение данных из PIN_SRV
 }
 
 void loop()
 {
-	l_phtrsr = analogRead(PIN_L_PHTRSR);
-	r_phtrsr = analogRead(PIN_R_PHTRSR);
+	l_phtrsr = analogRead(PIN_L_PHTRSR); // определение переменной l_phtrsr на получение данных из PIN_L_PHTRSR
+	r_phtrsr = analogRead(PIN_R_PHTRSR); // определение переменной r_phtrsr на получение данных из PIN_R_PHTRSR
 
-	l_phtrsr = map(l_phtrsr, 0, 1023, 0, SENSITIVITY);
-	r_phtrsr = map(r_phtrsr, 0, 1023, 0, SENSITIVITY);
+	l_phtrsr = map(l_phtrsr, 0, 1023, 0, SENSITIVITY); // настройка диапазона значений l_phtrsr
+	r_phtrsr = map(r_phtrsr, 0, 1023, 0, SENSITIVITY); // настройка диапазона значений r_phtrsr
+	movesrv(l_phtrsr, r_phtrsr);
+}
 
-	while (l_phtrsr != r_phtrsr)
-		if (l_phtrsr > r_phtrsr && srv_position < 180)
+int srv_position = 0; // текущая позиция сервопривода
+
+void movesrv(int l, int r)
+{
+	while (l != r)
+		if (l > r && srv_position < 180)
 			_srv.write(++srv_position);
-		else if (l_phtrsr < r_phtrsr && srv_position >= 0)
+		else if (l < r && srv_position >= 0)
 			_srv.write(--srv_position);
 }
+
+/*
+		поменять алгоритм на
+			l_phtrsr = map(l_phtrsr, 0, 1023, 0, SENSITIVITY);
+			r_phtrsr = map(r_phtrsr, 0, 1023, 0, SENSITIVITY);
+
+			srv_position += (l_phtrsr - r_phtrsr)
+			! если значение положительное то влево
+			! если значение отрицательно то вправо
+
+		алгоритм не доработан так как значение srv_position может выйти за рамки
+*/
