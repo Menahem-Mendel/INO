1b4042b38ed0d89004fd4d92c0257d6329658377
mendel <mendelgel@gmail.com>
2019-06-26 15:14:03 -0700

    jj

--- tracker_v2.ino
+++ tracker_v2.ino
@@ -6,7 +6,8 @@
 #define PINRIGHTLDR A1  // пин ввода: правый фоторезистор
 #define PINSERVO 10		// пин вывода: на сервопривод
 #define SENSITIVITY 180 // чувствительность фоторезистора от 0 до n, чем ниже значение тем выше чувствительность
-#define DTIME 1000		// скорсть реакции
+#define SENSITIVITYBEFORE 3968
+#define DTIME 1000 // скорсть реакции
 
 Servo servo_10; // сервопривод
 
@@ -18,9 +19,9 @@ void setup()
 	pinMode(A1, INPUT);
 }
 
-int leftLDR;  // левый фоторезистор
-int rightLDR; // правый фоторезистор
-int servoPos; // позиция сервопривода
+int leftLDR;					// левый фоторезистор
+int rightLDR;					// правый фоторезистор
+int servoPos = servo_10.read(); // позиция сервопривода
 
 void loop()
 {
@@ -30,19 +31,20 @@ void loop()
 		leftLDR = analogRead(PINLEFTLDR);
 		rightLDR = analogRead(PINRIGHTLDR);
 
-		servoPos = servo_10.read();
-
 		// настройка диапазона значений
-		leftLDR = map(leftLDR, 0, 3968, 0, SENSITIVITY);
-		rightLDR = map(rightLDR, 0, 3968, 0, SENSITIVITY);
+		leftLDR = map(leftLDR, 0, SENSITIVITYBEFORE, 0, SENSITIVITY);
+		rightLDR = map(rightLDR, 0, SENSITIVITYBEFORE, 0, SENSITIVITY);
 
 		while (leftLDR != rightLDR)
 		{
 			leftLDR = analogRead(PINLEFTLDR);
 			rightLDR = analogRead(PINRIGHTLDR);
 
+			leftLDR = map(leftLDR, 0, SENSITIVITYBEFORE, 0, SENSITIVITY);
+			rightLDR = map(rightLDR, 0, SENSITIVITYBEFORE, 0, SENSITIVITY);
+
 			// если солнца мало, опустить
-			if (leftLDR < SENSITIVITY - SENSITIVITY / 3 && rightLDR < SENSITIVITY - SENSITIVITY / 3)
+			if (leftLDR > SENSITIVITY - SENSITIVITY / 3 && rightLDR > SENSITIVITY - SENSITIVITY / 3)
 			{
 				servo_10.write(0);
 				break;
@@ -53,12 +55,11 @@ void loop()
 			else if (leftLDR < rightLDR)
 				--servoPos;
 
-			if (servoPos == 0 || servoPos == 180)
+			if (servoPos < 0 || servoPos > 180)
 				break;
 
 			servo_10.write(servoPos);
 		}
-
 		delay(DTIME);
 	}
 	else
